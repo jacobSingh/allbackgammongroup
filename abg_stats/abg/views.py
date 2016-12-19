9ff9f0ce100332850d138d4f941ad6dc1d08e02c
Jacob Singh <jacobsingh@gmail.com>
2016-12-19 22:18:55 +0530

    Fixed player DB

--- abg_stats/abg/views.py
+++ abg_stats/abg/views.py
@@ -42,7 +42,7 @@ assets.register('all_css', css)
 def show_player_stats(player_name):
 
     player = {}
-
+    # @TODO: remove the all_but_champ stuff and paramaterize
     players = pd.read_csv(os.path.join(app.config['DATA_DIR'], 'all_but_champ/players_elo.csv'))
     matches = pd.read_csv(os.path.join(app.config['DATA_DIR'], 'all_but_champ/match_log.csv'))
     player_matches = matches[(matches['player1-name'] == player_name) | (matches['player2-name'] == player_name)]
@@ -50,7 +50,7 @@ def show_player_stats(player_name):
     player_matches['ELO'][player_matches['player2-name'] == player_name] = player_matches.loc[player_matches['player2-name'] == player_name]['player2_ELO']
     player_matches = player_matches[['match-updated-at', 'name', 'winner','loser', 'winner_elo_in', 'loser_elo', 'ELO']]
     player_matches.columns=['Date', 'Tournament', 'Winner', 'Loser', 'Winner ELO', 'Loser ELO', 'Player ELO']
-    player = dict(players.loc[player_name])
+    player = dict(players[players["player_name"] == player_name].iloc[0])
 
     # @TODO: fix this column name
     player['name'] = player['player_name']
@@ -64,17 +64,6 @@ def show_player_stats(player_name):
     chart.buildhtml()
 
     return render_template('player.html', player=player, match_table=match_table, elo_chart=Markup(chart.htmlcontent))
-
-
-def get_main_elo_table_html(df):
-    df = df[['player_name', 'ELO', 'xp']]
-    df.columns = ['Player', 'ELO', 'Experience']
-    formatters = {
-        'Player': lambda x: '<a href="{}">{}</a>'.format(url_for('abg.show_player_stats', player_name=x), x)
-    }
-    pd.set_option('display.max_colwidth', 100)
-    return df.to_html(formatters=formatters, escape=False, float_format='%.0f')
-
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def home():
@@ -99,3 +88,38 @@ def home():
     }
 
     return render_template('abg.html', **abg_vars)
+
+@blueprint.route('/dqs', methods=['GET', 'POST'])
+def dqs():
+    """DQ page"""
+    app = flask.current_app
+    players = pd.read_csv(os.path.join(app.config['DATA_DIR'], 'all_but_champ/players_elo.csv'))
+    experienced_players = players.loc[players['xp'] >= app.config['XP_THRESHOLD']]
+    elo_table = get_main_elo_table_html(experienced_players)
+
+    funnynames = ['statocopia', 'lies and damn lies', 'nerdotica!', 'useless facts and figures']
+
+    img = BytesIO()
+    chart = sns.lmplot('ELO', 'xp', data=experienced_players, fit_reg=False)
+    chart.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue())
+
+    abg_vars = {
+        'sillyname': random.choice(funnynames),
+        'table': Markup(elo_table),
+        'scatter': Markup('<img src="data:image/png;base64,{}" />'.format(plot_url.decode('utf-8')))
+    }
+
+    return render_template('abg.html', **abg_vars)
+
+
+
+def get_main_elo_table_html(df):
+    df = df[['player_name', 'ELO', 'xp']]
+    df.columns = ['Player', 'ELO', 'Experience']
+    formatters = {
+        'Player': lambda x: '<a href="{}">{}</a>'.format(url_for('abg.show_player_stats', player_name=x), x)
+    }
+    pd.set_option('display.max_colwidth', 100)
+    return df.to_html(formatters=formatters, escape=False, float_format='%.0f')
