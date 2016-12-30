3df795806d9f145c02c6a652b3720ec71383881a
Jacob Singh <jacobsingh@gmail.com>
2016-12-31 01:36:05 +0530

    Fixed col width

--- abg_stats/abg/views/player.py
+++ abg_stats/abg/views/player.py
@@ -107,6 +107,7 @@ def show_player_stats(player_name):
         #"Win percentage": lambda x: "{}%".format(x)
     }
 
+    pd.set_option('display.max_colwidth', 100)
     top_opponents = Markup(df.head(20).to_html(escape=False,formatters = formatters))
 
     return render_template('player.html', player=player, match_table=match_table, elo_chart=chart, elo_stddev_chart=elo_stddev_chart, top_opponents=top_opponents)
